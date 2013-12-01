acefc60a97d7529f08204ce875f376d3d6157a96
Jinru Wu <woodbleak@gmail.com>
2013-11-30 21:46:57 -0500

    Merge changes

--- app/assets/javascripts/howtobealone.js
+++ app/assets/javascripts/howtobealone.js
@@ -1,4 +1,7 @@
-var htbaApp = angular.module('htbaApp', ['ngResource']);
+var htbaApp = angular.module('htbaApp', ['ngResource']).config(function($httpProvider) {
+		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+    }
+);
 
 htbaApp.controller('MeditationsCtrl', ['$scope', 'Meditation', function($scope, Meditation) {
 	$scope.heading = "How to Be Alone";
@@ -13,6 +16,15 @@ htbaApp.factory('Feeling', ['$resource', function($resource) {
 	return $resource('/feelings');
 }]);
 
+htbaApp.factory('MeditationService', function(m) {
+	Meditation.save(m, function(resource) {
+		console.log(resource);
+		return newMeditationInstance;
+	}, function(response) {
+		console.log("Error: " + response.status);
+	});
+});
+
 htbaApp.controller('FeelingsCtrl', ['$scope', 'Feeling', function($scope, Feeling){
 	$scope.create = function () {
 		Feeling.save($scope.newFeeling, function(resource) {
@@ -25,12 +37,23 @@ htbaApp.controller('FeelingsCtrl', ['$scope', 'Feeling', function($scope, Feelin
 
 	$scope.feelings = [
 		{name: 'Happy'},
+		{name: 'Normal'},
+		{name: 'Excited'},
 		{name: 'Angry' },
+		{name: 'Tired'},
 		{name: 'Relaxed'},
+		{name: 'Stressed'},
+		{name: 'Terrible'},
 		{name: 'Sad'}
 	];
 	$scope.feeling = $scope.feelings[0];
 	$scope.selectFeeling = function(f) {
-		console.log(f);
+		$scope.newFeeling = { feeling_desc: f.name };
+		// $scope.newFeeling.feeling_desc = f.name;
+		Feeling.save($scope.newFeeling, function(resource) {
+	      console.log(resource)
+	    }, function(response) {
+	      console.log("Error: " + response.status);
+	    });
 	};
 }]);
--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,3 +1,14 @@
 class FeelingsController < ApplicationController
-	respond_to :json
+	respond_to :html
+
+	def create
+		respond_to do |format|
+			format.html { render :json => Feeling.create(feeling_params) }
+		end
+	end
+
+	private
+	def feeling_params
+      params.require(:feeling).permit(:feeling_desc)
+    end
 end
